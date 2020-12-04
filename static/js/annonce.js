031392a3550dbf537f231481bfac5ee98d2828b1
loloula <43748725+loloula@users.noreply.github.com>
2020-12-04 16:27:39 +0100

    Add files via upload

--- static/js/annonce.js
+++ static/js/annonce.js
@@ -1,22 +1,21 @@
-
 document.addEventListener('DOMContentLoaded', () => {
+  //console.log("page");
   // debut script recuperer les annonces en cliquant sur un bouton
   const emp = document.getElementById("liste_annonces");
   const leBoutonliste = document.getElementById("bouton_liste");
   leBoutonliste.addEventListener("click", (evt) => {
         //recupérer la recheche saisie
   var recu = document.getElementById("recup").value;
-        //console.log(recu); teste
+  //console.log("sdfghj");
   const requeteHTTPGet = new XMLHttpRequest();
   requeteHTTPGet.addEventListener("load", (evt) => {
   const jsonData = evt.target.responseText;
                     // console.log("résultat du webservice reçu", jsonData);
   const data = JSON.parse(jsonData);
                     // console.log("données décodées", data);
-                    // vidage des commentaires déjà affichés
-  emp.innerHTML = "";
+
   for (const annonce of data) {
-          emp.innerHTML +='<div> <h5>'+annonce.titre+'</h5>'+
+          emp.innerHTML +=' <div ><h5>'+annonce.titre+'</h5>'+
           '<br> <a href="#">description </a>: '+annonce.description+
           '<br><a href="#"> catégorie</a> : '+annonce.categorie+
           '<br><a href="#"> pseudo </a>: '+annonce.pseudo+
@@ -24,13 +23,42 @@ document.addEventListener('DOMContentLoaded', () => {
           '<br><a href="#"> photo </a>: <img id="list" src="img/'+annonce.photo+'">'+
           '<br><a href="#"> rdv_lat </a>: '+annonce.rdv_lat+
           '<br><a href="#"> rdv_lon </a>: '+annonce.rdv_lon+
-          '<br><a href="#"> date </a>: '+annonce.date+'</div>';
-  }
+          '<br><a href="#"> date </a>: '+annonce.date+'</div> ';
+          emp.innerHTML += '<div> <button type="submit" class="supprimer btn btn-primary mb-2" data-idannonce='+annonce.id+'>supprimer</button></div>';
+
+  };
+  const supprim = document.getElementsByClassName("supprimer");
+  //console.log(supprim); teste
+  for (const monbuton of supprim){
+    const idannonce = monbuton.dataset.idannonce;
+    monbuton.addEventListener("click", (evt) => {
+        console.log(idannonce);
+        const requeteHTTPGet = new XMLHttpRequest();
+        requeteHTTPGet.open("get", "ws_supprimer_annonce.php?id="+idannonce);
+        requeteHTTPGet.send();
+    });
+
+  };
+
   });
                 //recuperer les données du webservices avec comme parametre le texte recuperer sur la zone de saisi
   requeteHTTPGet.open("get", "ws_liste_annonces.php?param="+recu);
   requeteHTTPGet.send();
+
   });
+
     //fin script affiche annonces
+    //debut script ajout d'une annonce
+    //pointer le formulaire de par son id
+    const formajoutannonce = document.getElementById("ajout_annonce");
+    formajoutannonce.addEventListener("submit", (event) => {
+           const requeteHTTPPost = new XMLHttpRequest();
+           //recuperer les donnees du formulaire
+           const donneesenvoyer = new FormData(formajoutannonce);
+           //appeler le web service
+           requeteHTTPPost.open("post", "ws_ajout_annonces.php");
+           requeteHTTPPost.send(donneesenvoyer);
+           event.preventDefault();
+ });
 
 });
---
+++ static/js/annonce1.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("page");
+  //pointer vers formulaire de connexion
+const formconnect = document.getElementById("connect");
+formconnect.addEventListener("submit", (event) => {
+    console.log("dfghjklm");
+
+});
+});
